be4007fc98f686007d71ebb302ba9337f4cbeb9c
Akash Mitra <akash.mitra@outlook.com>
2025-07-09 21:35:33 +0530

    feat: integrate shadcn ui

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaSave, FaUpload, FaDownload } from 'react-icons/fa';
+import { Button } from './components/ui/button';
 const Header = ({ onSave, onLoad, onExport }) => {
   return (
     <header>
@@ -8,9 +9,9 @@ const Header = ({ onSave, onLoad, onExport }) => {
           <button onClick={onSave}><FaSave /> Save</button>
           <button onClick={onLoad}><FaUpload /> Load</button>
           <button onClick={onExport}><FaDownload /> Export</button>
+          <Button className="ml-2">Shadcn</Button>
         </div>
     </header>
   );
 };
-
 export default Header;
